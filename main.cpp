269cd45d4a3fffaf3a928536cffebb6c73c67bf9
bigdatacon <link17892020@gmail.com>
2024-07-14 01:42:07 +0300

    Описание изменений

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "qcustomplot.h"
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include <iostream>
 
 using json = nlohmann::json;
 
@@ -17,6 +18,22 @@ struct Operation {
     int start;
     int finish;
 };
+
+QVector<Operation> jsOperations;
+QVector<Operation> msOperations;
+
+QLineEdit *startTimeEdit;
+QLineEdit *finishTimeEdit;
+QLineEdit *jobEdit;
+QLineEdit *machineEdit;
+
+QCPBars *barsJobs;
+QCPBars *barsMachines;
+
+QCustomPlot *plot1;
+QCustomPlot *plot2;
+
+Operation *selectedOperation = nullptr;
 
 void setupPlot(QCustomPlot *customPlot, const QVector<Operation> &operations, bool groupByJob) {
     QMap<int, int> uniqueLabels;
@@ -40,7 +57,7 @@ void setupPlot(QCustomPlot *customPlot, const QVector<Operation> &operations, bo
 
     QSharedPointer<QCPAxisTickerText> textTicker(new QCPAxisTickerText);
     for (auto it = uniqueLabels.begin(); it != uniqueLabels.end(); ++it) {
-        textTicker->addTick(it.value(), QString("J%1").arg(it.key()));
+        textTicker->addTick(it.value(), QString(groupByJob ? "J%1" : "M%1").arg(it.key()));
     }
 
     customPlot->yAxis->setTicker(textTicker);
@@ -49,6 +66,55 @@ void setupPlot(QCustomPlot *customPlot, const QVector<Operation> &operations, bo
     customPlot->xAxis->setRange(0, 300);
     customPlot->yAxis->setRange(-1, index);
     customPlot->replot();
+
+    if (groupByJob) {
+        barsJobs = bars;
+    } else {
+        barsMachines = bars;
+    }
+}
+
+void plotGraphs() {
+    plot1->clearPlottables();
+    plot2->clearPlottables();
+
+    setupPlot(plot1, jsOperations, false);
+    setupPlot(plot2, jsOperations, true);
+}
+
+void onBarClicked(QMouseEvent *event, QCustomPlot *customPlot) {
+    int index = -1;
+    QCPBars *bars = nullptr;
+
+    if (customPlot == plot1) {
+        bars = barsMachines;
+    } else if (customPlot == plot2) {
+        bars = barsJobs;
+    }
+
+    if (bars) {
+        double y = customPlot->yAxis->pixelToCoord(event->pos().y());
+        double key = round(y);
+        index = bars->data()->findBegin(key)->key;
+
+        if (index >= 0 && index < jsOperations.size()) {
+            selectedOperation = &jsOperations[index];
+            startTimeEdit->setText(QString::number(selectedOperation->start));
+            finishTimeEdit->setText(QString::number(selectedOperation->finish));
+            jobEdit->setText(QString::number(selectedOperation->job));
+            machineEdit->setText(QString::number(selectedOperation->machine));
+        }
+    }
+}
+
+void updateBar() {
+    if (selectedOperation) {
+        selectedOperation->start = startTimeEdit->text().toInt();
+        selectedOperation->finish = finishTimeEdit->text().toInt();
+        selectedOperation->job = jobEdit->text().toInt();
+        selectedOperation->machine = machineEdit->text().toInt();
+        plotGraphs();
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -57,27 +123,39 @@ int main(int argc, char *argv[]) {
 
     // Загрузка данных из JSON файла
     std::ifstream file("data.json");
-    json data;
-    file >> data;
-    file.close();
-
-    QVector<Operation> jsOperations;
-    QVector<Operation> msOperations;
-
-    for (const auto &op : data["js_operations"]) {
-        jsOperations.append({op["Job"], op["Machine"], op["Start"], op["Finish"]});
+    if (!file) {
+        std::cerr << "Error: Unable to open file data.json" << std::endl;
+        return 1;
     }
 
-    for (const auto &op : data["ms_operations"]) {
-        msOperations.append({op["Job"], op["Machine"], op["Start"], op["Finish"]});
+    json data;
+    try {
+        file >> data;
+    } catch (json::parse_error &e) {
+        std::cerr << "JSON parse error: " << e.what() << std::endl;
+        return 1;
+    }
+    file.close();
+
+    try {
+        for (const auto &op : data["js_operations"]) {
+            jsOperations.append({op["Job"], op["Machine"], op["Start"], op["Finish"]});
+        }
+
+        for (const auto &op : data["ms_operations"]) {
+            msOperations.append({op["Job"], op["Machine"], op["Start"], op["Finish"]});
+        }
+    } catch (json::type_error &e) {
+        std::cerr << "JSON type error: " << e.what() << std::endl;
+        return 1;
     }
 
     // Создание виджетов
     QWidget *centralWidget = new QWidget;
     QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
 
-    QCustomPlot *plot1 = new QCustomPlot;
-    QCustomPlot *plot2 = new QCustomPlot;
+    plot1 = new QCustomPlot;
+    plot2 = new QCustomPlot;
     mainLayout->addWidget(plot1);
     mainLayout->addWidget(plot2);
 
@@ -94,10 +172,10 @@ int main(int argc, char *argv[]) {
     labelsLayout->addWidget(machineLabel);
 
     QVBoxLayout *inputsLayout = new QVBoxLayout;
-    QLineEdit *startTimeEdit = new QLineEdit;
-    QLineEdit *finishTimeEdit = new QLineEdit;
-    QLineEdit *jobEdit = new QLineEdit;
-    QLineEdit *machineEdit = new QLineEdit;
+    startTimeEdit = new QLineEdit;
+    finishTimeEdit = new QLineEdit;
+    jobEdit = new QLineEdit;
+    machineEdit = new QLineEdit;
     inputsLayout->addWidget(startTimeEdit);
     inputsLayout->addWidget(finishTimeEdit);
     inputsLayout->addWidget(jobEdit);
@@ -119,11 +197,27 @@ int main(int argc, char *argv[]) {
     window.setCentralWidget(centralWidget);
 
     // Установка графиков
-    setupPlot(plot1, jsOperations, false);
-    setupPlot(plot2, jsOperations, true);
+    plotGraphs();
+
+    QObject::connect(applyButton, &QPushButton::clicked, updateBar);
+    QObject::connect(resetButton, &QPushButton::clicked, []() {
+        plotGraphs();
+        startTimeEdit->clear();
+        finishTimeEdit->clear();
+        jobEdit->clear();
+        machineEdit->clear();
+    });
 
     window.resize(800, 600);
     window.show();
+
+    QObject::connect(plot1, &QCustomPlot::mousePress, [plot1](QMouseEvent *event) {
+        onBarClicked(event, plot1);
+    });
+
+    QObject::connect(plot2, &QCustomPlot::mousePress, [plot2](QMouseEvent *event) {
+        onBarClicked(event, plot2);
+    });
 
     return a.exec();
 }
