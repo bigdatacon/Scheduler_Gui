3a66b23da4156a79f39289547566502c19300ea8
bigdatacon <link17892020@gmail.com>
2024-07-24 13:18:33 +0300

    отрисовка

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -37,7 +37,9 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
 void MainWindow::mouseMoveEvent(QMouseEvent *event)
 {
     if (selectedBar) {
+        int dx = event->pos().x() - lastMousePos.x();
         int dy = event->pos().y() - lastMousePos.y();
+        selectedBar->rect.moveLeft(selectedBar->rect.left() + dx);
         selectedBar->rect.moveTop(selectedBar->rect.top() + dy);
         lastMousePos = event->pos();
         updateBarChart();
@@ -59,6 +61,20 @@ void MainWindow::drawBarChart(QPixmap &pixmap)
     painter.setPen(QPen(Qt::black, 2));
     painter.drawLine(50, 50, 50, pixmap.height() - 50); // Y axis
     painter.drawLine(50, pixmap.height() - 50, pixmap.width() - 50, pixmap.height() - 50); // X axis
+
+    // Draw X axis labels
+    int xLabelInterval = 50;
+    for (int i = 50; i < pixmap.width() - 50; i += xLabelInterval) {
+        painter.drawLine(i, pixmap.height() - 55, i, pixmap.height() - 45);
+        painter.drawText(i - 10, pixmap.height() - 30, QString::number((i - 50) / 10));
+    }
+
+    // Draw Y axis labels
+    int yLabelInterval = 50;
+    for (int i = pixmap.height() - 50; i > 50; i -= yLabelInterval) {
+        painter.drawLine(45, i, 55, i);
+        painter.drawText(20, i + 5, QString::number((pixmap.height() - 50 - i) / 10));
+    }
 
     int barWidth = 50;
     int spacing = 20;
@@ -88,6 +104,20 @@ void MainWindow::updateBarChart()
     painter.setPen(QPen(Qt::black, 2));
     painter.drawLine(50, 50, 50, newPixmap.height() - 50); // Y axis
     painter.drawLine(50, newPixmap.height() - 50, newPixmap.width() - 50, newPixmap.height() - 50); // X axis
+
+    // Draw X axis labels
+    int xLabelInterval = 50;
+    for (int i = 50; i < newPixmap.width() - 50; i += xLabelInterval) {
+        painter.drawLine(i, newPixmap.height() - 55, i, newPixmap.height() - 45);
+        painter.drawText(i - 10, newPixmap.height() - 30, QString::number((i - 50) / 10));
+    }
+
+    // Draw Y axis labels
+    int yLabelInterval = 50;
+    for (int i = newPixmap.height() - 50; i > 50; i -= yLabelInterval) {
+        painter.drawLine(45, i, 55, i);
+        painter.drawText(20, i + 5, QString::number((newPixmap.height() - 50 - i) / 10));
+    }
 
     for (const Bar &bar : bars) {
         bool overlap = false;
